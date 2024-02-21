c6ce747c8d03461b20a6594b361059afac6ec2e1
Alex Schmith <alexanderschmith@protonmail.com>
2024-02-21 12:34:46 -0500

    Create convertArticle.hs

---
+++ convertArticle.hs
@@ -0,0 +1,16 @@
+import Data.List 
+
+
+markdownToHtml :: String -> String
+
+convertLine :: String -> String
+
+article :: String -> String
+
+metadata :: String -> String
+
+header :: String -> String
+
+image :: String -> String
+
+code :: String -> String
