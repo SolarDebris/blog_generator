b07de36c3f5c6b7db381e2827f77ca66ef27964f
Alex Schmith <alexanderschmith@protonmail.com>
2024-04-07 17:45:52 -0400

    Added some more functions

--- markdownToHtml.hs
+++ markdownToHtml.hs
@@ -3,7 +3,8 @@ import System.IO
 import System.Directory
 
 
-
+-- A custom data structure for
+-- storing the metadata
 data Metadata = Metadata 
     { 
         title :: String,
@@ -12,23 +13,29 @@ data Metadata = Metadata
         description :: String
     }
 
+-- Function that will split document by the string "---"
+-- and takes the header and document
 splitDocument :: String -> (String, String)
 splitDocument markdown = 
     let splitted = split (onSublist "---") markdown
     in (splitted !! 2, splitted !! 4)
 
-parseMetadata :: String -> Metadata
-parseMetadata :: 
 
+-- Function that will use the results of splitDocument to parse the metadata
+-- and parse the document
+-- (NOTE this function may be useless)
+parseDocument :: (String, String) -> [String]
+parseDocument (header, document) = parseMetadata header
 
+-- Function that takes a string and parses it into the Metadata class
+parseMetadata :: String -> [String]
+parseMetadata header = tail (lines header)
 
 
 main :: IO()
 main = do
     withFile "articles/defcon31q.md" ReadMode (\handle -> do
         markdown <- hGetContents handle   
-        print (splitDocument markdown)
+        print (parseDocument(splitDocument markdown))
         )
     
-
-
